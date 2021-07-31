371a98534cbe4d07d70b2f0cecc1e9d6310f63f6
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-07-31 10:51:32 +0300

    Update resource folder nesting
    Update resource folder nesting
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/player/hand.py
+++ src/player/hand.py
@@ -4,8 +4,8 @@ class Hand(Entity):
     def __init__(self):
         super().__init__(
             parent = camera.ui, # Specifies parent of hand which is the player
-            model = '/resources/arm',
-            texture = '/resources/arm_texture.png',
+            model = './resources/arm',
+            texture = './resources/arm_texture.png',
             scale = 0.2,
             rotation = Vec3(160,-5,0),
             position = Vec2(0.5,-0.6)
