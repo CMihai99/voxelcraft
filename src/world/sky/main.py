7ff98b63f9933e9e8fb7df64fc4fe058c5c295aa
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-07-31 10:51:27 +0300

    Update resource folder nesting
    Update resource folder nesting
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/world/sky/main.py
+++ src/world/sky/main.py
@@ -5,7 +5,7 @@ class Sky(Entity):
         super().__init__(
             parent = scene,     # Specifies sky parent to scale properly
             model = 'sphere',
-            texture = '/resources/skybox.jpg',
+            texture = '../resources/skybox.jpg',
             scale = 1000,
             double_sided = True # See the sphere when you are inside it
         )
