32dae87d2dc6f61252a9a575bb9fcdfaf7f3b438
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-07-31 10:36:22 +0300

    Update sky.py code spacing
    Update sky.py code spacing
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/sky.py
+++ src/sky.py
@@ -3,9 +3,9 @@ from ursina import *
 class Sky(Entity):
     def __init__(self):
         super().__init__(
-            parent = scene,     # Specifies parent of sky so it scales properly
+            parent = scene,     # Specifies sky parent to scale properly
             model = 'sphere',
             texture = '/resources/skybox.jpg',
-            scale = 1000,       # Increases size drastically
-            double_sided = True # See the sphere when you are in it
+            scale = 1000,
+            double_sided = True # See the sphere when you are inside it
         )
