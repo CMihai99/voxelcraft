1e26c030a044fa94d8694c09929fd420f9e1c4ab
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-07-31 12:16:45 +0300

    Rename hand.py to arm.py
    Rename hand.py to arm.py
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/player/arm.py
+++ src/player/arm.py
@@ -1,17 +1,17 @@
 from ursina import *
 
-class Hand(Entity):
+class Arm(Entity):
     def __init__(self):
         super().__init__(
-            parent = camera.ui, # Specifies parent of hand which is the player
-            model = './resources/arm',
-            texture = './resources/arm_texture.png',
+            parent = camera.ui, # Specifies parent of the arm which is the player
+            model = './resources/player/arm/arm',
+            texture = './resources/player/arm/arm_texture.png',
             scale = 0.2,
             rotation = Vec3(160,-5,0),
             position = Vec2(0.5,-0.6)
         )
 
-    # Hand animations
+    # Arm animations
     def active(self):
         self.rotation = Vec3(160,-5,0)
         self.position = Vec2(0.4,-0.5)
