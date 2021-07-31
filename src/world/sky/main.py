1ede924058ec2e9d081375d21e8458a7ca34f4bf
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-07-31 12:22:26 +0300

    Update skybox file type
    Update skybox file type
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/world/sky/main.py
+++ src/world/sky/main.py
@@ -5,7 +5,7 @@ class Sky(Entity):
         super().__init__(
             parent = scene,     # Specifies sky parent to scale properly
             model = 'sphere',
-            texture = '../resources/world/sky/skybox.jpg',
+            texture = '../resources/world/sky/skybox.png',
             scale = 1000,
             double_sided = True # See the sphere when you are inside it
         )
