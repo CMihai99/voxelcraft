69b9ef69047b2a71fe1c99f2278ee9bb55953e90
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-07-31 14:56:03 +0300

    Change sky color and shape
    Change sky color and shape
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/world/sky/main.py
+++ src/world/sky/main.py
@@ -4,8 +4,8 @@ class Sky(Entity):
     def __init__(self):
         super().__init__(
             parent = scene,     # Specifies sky parent to scale properly
-            model = 'sphere',
-            texture = '../resources/world/sky/skybox.png',
+            model = 'cube',
+            color = color.rgb(115, 180, 255),
             scale = 1000,
             double_sided = True # See the sphere when you are inside it
         )
