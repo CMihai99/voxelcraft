93dc870cd8de0ede2b374063366ae502d22ab0b3
Calinescu Mihai <mihaimihaia431@gmail.com>
2021-11-19 20:00:37 +0200

    Cleanup `main.py`
    Cleanup main.py
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/world/sky/main.py
+++ src/world/sky/main.py
@@ -12,9 +12,9 @@ from ursina import *
 class Sky(Entity):
     def __init__(self):
         super().__init__(
-            parent = scene,     # Specifies sky parent to scale properly
+            parent = scene,
             model = 'cube',
             color = color.rgb(115, 180, 255),
             scale = 1000,
-            double_sided = True # See the sphere when you are inside it
+            double_sided = True # See the sky when you are inside it
         )
