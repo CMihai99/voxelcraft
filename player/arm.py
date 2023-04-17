404cacd65c97996b0346651aea936a548edfb97d
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-17 22:06:24 +0300

    Update Copyright year, add commentary & new scale
    Update 'player/arm.py' Copyright year from '2021' to '2023'
    Add code commentary
    Change scale attribute from '0.2' to '0.15' 
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- player/arm.py
+++ player/arm.py
@@ -1,21 +1,22 @@
 '''
-===-----------------------------------------------------------------------------------===
-Copyright (c) 2021 Voxelcraft
+-----------------------------------------------------------------------------------------
+Copyright (c) 2023 Voxelcraft
 
 For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
 For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
-===-----------------------------------------------------------------------------------===
+-----------------------------------------------------------------------------------------
 '''
 
+# Import module
 from ursina import *
 
 class Arm(Entity):
 	def __init__(self):
 		super().__init__(
 			parent = camera.ui,
-			model='../resources/player/arm/models/arm.obj',
-			texture = "../resources/player/arm/textures/arm.png",
-			scale = 0.2,
+			model= '../resources/player/arm/models/arm.obj',
+			texture = '../resources/player/arm/textures/arm.png',
+			scale = 0.15,
 			rotation = Vec3(150,-10,0),
 			position = Vec2(0.4,-0.6))
 
