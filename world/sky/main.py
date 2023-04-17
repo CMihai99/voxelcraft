5a96a69c13badc223b56a9fa5cd963f706d23ca7
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-17 21:56:13 +0300

    Update Copyright year, add commentary and texture
    Update `world/sky/main.py` Copyright year from '2021' to '2023'
    Add and improve code commentary
    Add texture attribute (commented-out for now)
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- world/sky/main.py
+++ world/sky/main.py
@@ -1,12 +1,13 @@
 '''
-===-----------------------------------------------------------------------------------===
-Copyright (c) 2021 Voxelcraft
+-----------------------------------------------------------------------------------------
+Copyright (c) 2023 Voxelcraft
 
 For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
 For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
-===-----------------------------------------------------------------------------------===
+-----------------------------------------------------------------------------------------
 '''
 
+# Import modules
 from ursina import *
 
 class Sky(Entity):
@@ -14,7 +15,8 @@ class Sky(Entity):
         super().__init__(
             parent = scene,
             model = 'cube',
-            color = color.rgb(115, 180, 255),
+            # texture = "../resources/world/sky/textures/sky.png",
+            color = color.rgb(95, 175, 255),
             scale = 1000,
-            double_sided = True # See the sky when you are inside it
+            double_sided = True # See sky when inside it
         )
