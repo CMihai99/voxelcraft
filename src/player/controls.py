64cc7efe798fdc7834a2596d6090547159fe75c8
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-20 23:49:52 +0300

    Add `src/player/controls.py`
    Add 'src/player/controls.py'
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

---
+++ src/player/controls.py
@@ -0,0 +1,30 @@
+'''
+-----------------------------------------------------------------------------------------
+Copyright (c) 2023 Voxelcraft
+
+For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
+For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
+-----------------------------------------------------------------------------------------
+'''
+
+# Import module
+from ursina import *
+
+# Import file components
+from player import hit_sound
+from arm import Arm
+
+class PlayerControls(Entity):
+    def input(self, key):
+        if self.hovered:
+            # If left mouse button is pressed
+            if key == 'left mouse down':
+                Arm().active() # Arm animation is active
+
+                hit_sound.play() # Play hit sound
+            else:
+                Arm().passive() # Arm animation is passive
+
+            # If right mouse button is pressed
+            if key == 'right mouse down':
+                hit_sound.play() # Play hit sound
