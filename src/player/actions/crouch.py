ce9a7669d813784e3a6b4e800be1c00800b82398
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 16:02:44 +0300

    Format code using Black
    Format code using Black, the uncompromising Python code formatter
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/player/actions/crouch.py
+++ src/player/actions/crouch.py
@@ -1,11 +1,11 @@
-'''
+"""
 -----------------------------------------------------------------------------------------
 Copyright (c) 2023 Voxelcraft
 
 For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
 For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
 -----------------------------------------------------------------------------------------
-'''
+"""
 
 # Import module
 from ursina import *
@@ -16,21 +16,26 @@ from player.actions.walk import walk_speed
 
 crouch_speed = walk_speed / 5
 
+
 class Crouch(Entity):
     def __init__(self):
         super().__init__(
-            parent = camera.ui
-            )
+            parent=camera.ui
+        )
 
     def update(self):
         # If 'shift' key is held and player isn't walking or sprinting,
         # player is crouching
-        if held_keys['shift'] and ursina_player.walk is False and ursina_player.sprint is False:
+        if (
+            held_keys["shift"]
+            and ursina_player.walk is False
+            and ursina_player.sprint is False
+        ):
             ursina_player.crouch = True
-            ursina_player.speed = lerp(walk_speed, crouch_speed, 0.5) # Smooth speed transition
-            ursina_player.origin_y = ursina_player.origin_y - 1 # Crouch 1 block
+            ursina_player.speed = lerp(walk_speed, crouch_speed, 0.5)  # Smooth speed transition
+            ursina_player.origin_y = ursina_player.origin_y - 1  # Crouch 1 block
         # Player isn't crouching
         else:
             ursina_player.crouch = False
-            ursina_player.speed = lerp(crouch_speed, walk_speed, 1) # Smooth speed transition
-            ursina_player.origin_y = ursina_player.origin_y # Default height
+            ursina_player.speed = lerp(crouch_speed, walk_speed, 1)  # Smooth speed transition
+            ursina_player.origin_y = ursina_player.origin_y  # Default height
