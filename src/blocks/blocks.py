5c7b9ce4e46b25975ca80f875a65882d8b2166d4
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 16:12:48 +0300

    Format code using Black
    Format code using Black, the uncompromising Python code formatter
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/blocks/blocks.py
+++ src/blocks/blocks.py
@@ -1,29 +1,30 @@
-'''
+"""
 -----------------------------------------------------------------------------------------
 Copyright (c) 2023 Voxelcraft
 
 For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
 For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
 -----------------------------------------------------------------------------------------
-'''
+"""
 
 # Import module
 from ursina import *
 
 # Textures
-grass_texture = load_texture('/resources/blocks/textures/grass.png')
-dirt_texture = load_texture('/resources/blocks/textures/dirt.png')
-stone_texture = load_texture('/resources/blocks/textures/stone.png')
-cobblestone_texture = load_texture('/resources/blocks/textures/cobblestone.png')
+grass_texture = load_texture("/resources/blocks/textures/grass.png")
+dirt_texture = load_texture("/resources/blocks/textures/dirt.png")
+stone_texture = load_texture("/resources/blocks/textures/stone.png")
+cobblestone_texture = load_texture("/resources/blocks/textures/cobblestone.png")
 
 # Default block picked
-block_pick = 1 # Grass block
+block_pick = 1  # Grass block
+
 
 class Blocks(Entity):
     def __init__(self):
         super().__init__(
-            parent = scene,
-            model = '/resources/blocks/models/block.obj',
-            color = color.color(0, 0, random.uniform(0.9, 1)),
-            scale = 1
-            )
+            parent=scene,
+            model="/resources/blocks/models/block.obj",
+            color=color.color(0, 0, random.uniform(0.9, 1)),
+            scale=1,
+        )
