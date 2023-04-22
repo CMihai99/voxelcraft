bd1d93d14d1107e462cda960ed364c01b755e27d
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 15:52:10 +0300

    Format code using Black
    Format code using Black, the uncompromising Python code formatter
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/player/player.py
+++ src/player/player.py
@@ -1,21 +1,22 @@
-'''
+"""
 -----------------------------------------------------------------------------------------
 Copyright (c) 2023 Voxelcraft
 
 For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
 For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
 -----------------------------------------------------------------------------------------
-'''
+"""
 
 # Import module
 from ursina import *
 
 # Sound
-hit_sound = Audio('/resources/player/sounds/hit.wav', loop = False, autoplay = False)
+hit_sound = Audio("/resources/player/sounds/hit.wav", loop=False, autoplay=False)
+
 
 class Player(Entity):
     def __init__(self):
         super().__init__(
-            parent = camera.ui,
-            origin_y = 2.5 # 2.5 block height
+            parent=camera.ui,
+            origin_y=2.5  # 2.5 block height
         )
