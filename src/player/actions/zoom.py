a8a7548ae7aefadbcf8a25994499d7f41e88587c
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 15:55:06 +0300

    Format code using Black
    Format code using Black, the uncompromising Python code formatter
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/player/actions/zoom.py
+++ src/player/actions/zoom.py
@@ -1,11 +1,11 @@
-'''
+"""
 -----------------------------------------------------------------------------------------
 Copyright (c) 2023 Voxelcraft
 
 For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
 For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
 -----------------------------------------------------------------------------------------
-'''
+"""
 
 # Import module
 from ursina import *
@@ -13,15 +13,16 @@ from ursina import *
 # Import file component
 from main import ursina_player
 
+
 class Zoom(Entity):
     def __init__(self):
         super().__init__(
-            parent = camera.ui
-            )
+            parent=camera.ui
+        )
 
     def update(self):
         # If 'c' key is held, player is zooming
-        if held_keys['c']:
+        if held_keys["c"]:
             ursina_player.zoom = True
             camera.fov /= 1.33
             ursina_player.x /= 1.33
