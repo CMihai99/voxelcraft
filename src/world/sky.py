7b3d5f20a2416461eabcb0329e355d70f2a78433
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 15:49:17 +0300

    Format code using Black
    Format code using Black, the uncompromising Python code formatter
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/world/sky.py
+++ src/world/sky.py
@@ -1,21 +1,22 @@
-'''
+"""
 -----------------------------------------------------------------------------------------
 Copyright (c) 2023 Voxelcraft
 
 For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
 For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
 -----------------------------------------------------------------------------------------
-'''
+"""
 
 # Import module
 from ursina import *
 
+
 class Sky(Entity):
     def __init__(self):
         super().__init__(
-            parent = scene,
-            model = 'sphere',
-            texture = '/resources/world/sky/textures/sky.png',
-            scale = 1000,
-            double_sided = True # See sky if in it
-            )
+            parent=scene,
+            model="sphere",
+            texture="/resources/world/sky/textures/sky.png",
+            scale=1000,
+            double_sided=True  # See sky if in it
+        )
