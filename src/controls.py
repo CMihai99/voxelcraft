0228646f5a7572adcef391e8679423f8b2244902
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 15:24:34 +0300

    Format code using Black
    Format code using Black, the uncompromising Python code formatter
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/controls.py
+++ src/controls.py
@@ -1,18 +1,19 @@
-'''
+"""
 -----------------------------------------------------------------------------------------
 Copyright (c) 2023 Voxelcraft
 
 For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
 For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
 -----------------------------------------------------------------------------------------
-'''
+"""
 
 # Import module
 from ursina import *
+
 
 class MainControls(Entity):
     def input(self, key):
         if self.hovered:
             # If escape key is pressed, exit program
-            if key == 'escape':
+            if key == "escape":
                 exit()
