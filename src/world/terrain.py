b9c604f64e12767294ac9e61e18b5cc2d030b7f4
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 15:46:09 +0300

    Format code using Black
    Format code using Black, the uncompromising Python code formatter
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/world/terrain.py
+++ src/world/terrain.py
@@ -1,11 +1,11 @@
-'''
+"""
 -----------------------------------------------------------------------------------------
 Copyright (c) 2023 Voxelcraft
 
 For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
 For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
 -----------------------------------------------------------------------------------------
-'''
+"""
 
 # Import module
 from ursina import *
@@ -13,15 +13,16 @@ from ursina import *
 # Import file component
 from blocks.blocks import Blocks
 
-class Terrain(Entity):
-	def __init__(self):
-		super().__init__(
-			parent = scene,
-			double_sided = True # See terrain if in it
-			)
 
-	def generate(self):
-		for x in range(16): # 16 blocks on the x-axis
-			for y in range(4): # 4 blocks on the y-axis
-				for z in range(16): # 16 blocks on the z-axis
-					Blocks(position = (x, y, z), origin_y = 0)
+class Terrain(Entity):
+    def __init__(self):
+        super().__init__(
+            parent=scene,
+            double_sided=True  # See terrain if in it
+            )
+
+    def generate(self):
+        for x in range(16):  # 16 blocks on the x-axis
+            for y in range(4):  # 4 blocks on the y-axis
+                for z in range(16):  # 16 blocks on the z-axis
+                    Blocks(position=(x, y, z), origin_y=0)
