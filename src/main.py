e657f295743cbc247e7b3385b0e2bcffe7d245d8
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 15:23:27 +0300

    Format code using psf/black
    Format code using Black, the uncompromising Python code formatter
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-'''
+"""
 -----------------------------------------------------------------------------------------
 Copyright (c) 2023 Voxelcraft
 
 For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
 For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
 -----------------------------------------------------------------------------------------
-'''
+"""
 
 # Import module
 from ursina import *
@@ -23,8 +23,8 @@ game = Ursina()
 ursina_player = FirstPersonController()
 
 # Window settings
-window.title = 'Voxelcraft Beta 1.0.0'
-window.icon = load_texture('/resources/blocks/textures/grass.png')
+window.title = "Voxelcraft Beta 1.0.0"
+window.icon = load_texture("/resources/blocks/textures/grass.png")
 window.borderless = False
 window.fullscreen = False
 window.exit_button.visible = False
