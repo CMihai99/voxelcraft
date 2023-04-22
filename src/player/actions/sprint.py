e724454e1c04b4d2c84897aa4f30804b00ba7677
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 16:00:32 +0300

    Format code using Black
    Format code using Black, the uncompromising Python code formatter
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/player/actions/sprint.py
+++ src/player/actions/sprint.py
@@ -1,11 +1,11 @@
-'''
+"""
 -----------------------------------------------------------------------------------------
 Copyright (c) 2023 Voxelcraft
 
 For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
 For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
 -----------------------------------------------------------------------------------------
-'''
+"""
 
 # Import module
 from ursina import *
@@ -17,21 +17,26 @@ from player.actions.walk import walk_speed, walk_fov
 sprint_speed = walk_speed * 5
 sprint_fov = walk_fov + 20
 
+
 class Sprint(Entity):
     def __init__(self):
         super().__init__(
-            parent = camera.ui
-            )
+            parent=camera.ui
+        )
 
     def update(self):
-        # If 'control' and 'w' keys are held and player isn't walking or crouching,
-        # player is sprinting
-        if held_keys['control' and 'w'] and ursina_player.walk is False and ursina_player.crouch is False:
+        # If 'control' and 'w' keys are held and player isn't walking
+        # or crouching, player is sprinting
+        if (
+            held_keys["control" and "w"]
+            and ursina_player.walk is False
+            and ursina_player.crouch is False
+        ):
             ursina_player.sprint = True
-            ursina_player.speed = lerp(walk_speed, sprint_speed, 2) # Smooth speed transition
-            camera.fov = lerp(walk_fov, sprint_fov, 1) # Smooth FOV transition
+            ursina_player.speed = lerp(walk_speed, sprint_speed, 2)  # Smooth speed transition
+            camera.fov = lerp(walk_fov, sprint_fov, 1)  # Smooth FOV transition
         # Player isn't sprinting
         else:
             ursina_player.sprint = False
-            ursina_player.speed = lerp(sprint_speed, walk_speed, 4) # Smooth speed transition
-            camera.fov = lerp(sprint_fov, walk_fov, 2) # Smooth FOV transition
+            ursina_player.speed = lerp(sprint_speed, walk_speed, 4)  # Smooth speed transition
+            camera.fov = lerp(sprint_fov, walk_fov, 2)  # Smooth FOV transition
