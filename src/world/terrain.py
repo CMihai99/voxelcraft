8b39c4c2f91587059c944dde4548b712a4b3dddd
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 13:14:34 +0300

    Remove useless `sys` module code, update code commentary and set terrain generation to y(0)
    Remove useless 'sys' module code
    Update code commentary
    Set terrain generation to y(0)
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/world/terrain.py
+++ src/world/terrain.py
@@ -7,12 +7,8 @@ For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENS
 -----------------------------------------------------------------------------------------
 '''
 
-# Import modules
+# Import module
 from ursina import *
-import sys
-
-# Move beyond top level
-sys.path.append('..')
 
 # Import file component
 from blocks.blocks import Blocks
@@ -21,11 +17,11 @@ class Terrain(Entity):
 	def __init__(self):
 		super().__init__(
 			parent = scene,
-			double_sided = True # See terrain when in it
+			double_sided = True # See terrain if in it
 			)
 
 	def generate(self):
 		for x in range(16): # 16 blocks on the x-axis
 			for y in range(4): # 4 blocks on the y-axis
 				for z in range(16): # 16 blocks on the z-axis
-					Blocks(position = (x, y, z), origin_y = 2.25) # OR origin_y = 2.5
+					Blocks(position = (x, y, z), origin_y = 0)
