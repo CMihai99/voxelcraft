6025e9ed8f5e33b55ace6002ec3d1bdc62637e43
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 15:53:31 +0300

    Format code using Black
    Format code using Black, the uncompromising Python code formatter
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/player/arm.py
+++ src/player/arm.py
@@ -1,30 +1,31 @@
-'''
+"""
 -----------------------------------------------------------------------------------------
 Copyright (c) 2023 Voxelcraft
 
 For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
 For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
 -----------------------------------------------------------------------------------------
-'''
+"""
 
 # Import module
 from ursina import *
 
+
 class Arm(Entity):
-	def __init__(self):
-		super().__init__(
-			parent = camera.ui,
-			model = '/resources/player/arm/models/arm.obj',
-			texture = '/resources/player/arm/textures/arm.png',
-			scale = 0.15,
-			rotation = Vec3(150,-10,0),
-			position = Vec2(0.4,-0.6)
-			)
+    def __init__(self):
+        super().__init__(
+            parent=camera.ui,
+            model="/resources/player/arm/models/arm.obj",
+            texture="/resources/player/arm/textures/arm.png",
+            scale=0.15,
+            rotation=Vec3(150, -10, 0),
+            position=Vec2(0.4, -0.6)
+        )
 
-	# Arm position when active
-	def active(self):
-		self.position = Vec2(0.3,-0.5)
+    # Arm position when active
+    def active(self):
+        self.position = Vec2(0.3, -0.5)
 
-	# Arm position when passive
-	def passive(self):
-		self.position = Vec2(0.4,-0.6)
+    # Arm position when passive
+    def passive(self):
+        self.position = Vec2(0.4, -0.6)
