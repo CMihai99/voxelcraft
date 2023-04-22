cfba79cf70806b5b66cdbd81bafc56217eeaf6e6
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 13:27:31 +0300

    Add file component import and add `origin_y` customization variable which acts as player height
    Add file component import
    Add `origin_y` customization variable which acts as player height
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/player/player.py
+++ src/player/player.py
@@ -10,11 +10,15 @@ For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENS
 # Import module
 from ursina import *
 
+# Import file component
+from main import ursina_player
+
 # Sound
-hit_sound = Audio('.../resources/player/sounds/hit.wav', loop = False, autoplay = False)
+hit_sound = Audio('/resources/player/sounds/hit.wav', loop = False, autoplay = False)
 
 class Player(Entity):
     def __init__(self):
         super().__init__(
-            parent = camera.ui
-            )
+            parent = camera.ui,
+            origin_y = 2.5 # 2.5 block height
+        )
