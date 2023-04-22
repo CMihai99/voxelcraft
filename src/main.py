4ff150b8e064a2eb6138c413ceb57e8520dc6b5f
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 13:03:47 +0300

    Remove file component imports and import (future) file which will store them, change 'Ursina()' variable, add file extension to texture load
    Remove file component imports and import (future) file which will store them
    Change 'Ursina()' variable from 'app' to 'game'
    Add file extension to 'window.icon' texture load
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/main.py
+++ src/main.py
@@ -13,57 +13,22 @@ from ursina import *
 # Import module component
 from ursina.prefabs.first_person_controller import FirstPersonController
 
-# Import file components
-from world.sky import Sky
-from world.terrain import Terrain
+# Import file which stores file components
+from __init__ import *
 
-from blocks.blocks import Blocks
-from player.player import Player
-from player.arm import Arm
-from inventory.inventory import Inventory
+# Declare game
+game = Ursina()
 
-from player.actions.zooming import Zoom
-from player.actions.walking import Walk
-from player.actions.sprinting import Sprint
-from player.actions.crouching import Crouch
-
-from controls import MainControls
-from blocks.controls import BlocksControls
-from player.controls import PlayerControls
-from inventory.controls import InventoryControls
-
-# Declare program
-app = Ursina()
-
-# Map module component
+# Link module component
 ursina_player = FirstPersonController()
-
-# Map file components
-Sky()
-Terrain()
-
-Blocks()
-Player()
-Arm()
-Inventory()
-
-Zoom()
-Walk()
-Sprint()
-Crouch()
-
-MainControls()
-BlocksControls()
-PlayerControls()
-InventoryControls()
 
 # Window settings
 window.title = 'Voxelcraft Beta 1.0.0'
-window.icon = load_texture('../resources/blocks/textures/grass')
+window.icon = load_texture('/resources/blocks/textures/grass.png')
 window.borderless = False
 window.fullscreen = False
 window.exit_button.visible = False
 window.fps_counter.enabled = False
 
-# Run program
-app.run()
+# Run game
+game.run()
