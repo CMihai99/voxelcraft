ce78356927d08848c8bf36a792c431aa4bbfa810
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 15:54:14 +0300

    Format code using Black
    Format code using Black, the uncompromising Python code formatter
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/player/controls.py
+++ src/player/controls.py
@@ -1,11 +1,11 @@
-'''
+"""
 -----------------------------------------------------------------------------------------
 Copyright (c) 2023 Voxelcraft
 
 For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
 For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
 -----------------------------------------------------------------------------------------
-'''
+"""
 
 # Import module
 from ursina import *
@@ -14,17 +14,18 @@ from ursina import *
 from player.player import hit_sound
 from player.arm import Arm
 
+
 class PlayerControls(Entity):
     def input(self, key):
         if self.hovered:
             # If left mouse button is pressed
-            if key == 'left mouse down':
-                Arm().active() # Arm animation is active
+            if key == "left mouse down":
+                Arm().active()  # Arm animation is active
 
-                hit_sound.play() # Play hit sound
+                hit_sound.play()  # Play hit sound
             else:
-                Arm().passive() # Arm animation is passive
+                Arm().passive()  # Arm animation is passive
 
             # If right mouse button is pressed, play hit sound
-            if key == 'right mouse down':
+            if key == "right mouse down":
                 hit_sound.play()
