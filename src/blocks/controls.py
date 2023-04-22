937715ca06864ffca2c0611383b209fd4824159b
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 16:15:42 +0300

    Format code using Black
    Format code using Black, the uncompromising Python code formatter
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/blocks/controls.py
+++ src/blocks/controls.py
@@ -1,17 +1,23 @@
-'''
+"""
 -----------------------------------------------------------------------------------------
 Copyright (c) 2023 Voxelcraft
 For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
 For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
 -----------------------------------------------------------------------------------------
-'''
+"""
 
 # Import module
 from ursina import *
 
 # Import file components
-from blocks.blocks import grass_texture, dirt_texture, stone_texture, cobblestone_texture
+from blocks.blocks import (
+    grass_texture,
+    dirt_texture,
+    stone_texture,
+    cobblestone_texture,
+)
 from blocks.blocks import block_pick, Blocks
+
 
 class BlocksControls(Entity):
     def input(self, key):
@@ -19,29 +25,41 @@ class BlocksControls(Entity):
 
         if self.hovered:
             # If 'number 1' key is pressed
-            if key == '1':
-                block_pick = 1 # Pick grass block
+            if key == "1":
+                block_pick = 1  # Pick grass block
             # If 'number 2' key is pressed
-            if key == '2':
-                block_pick = 2 # Pick dirt block
+            if key == "2":
+                block_pick = 2  # Pick dirt block
             # If 'number 3' key is pressed
-            if key == '3':
-                block_pick = 3 # Pick stone block
+            if key == "3":
+                block_pick = 3  # Pick stone block
             # If 'number 4' key is pressed
-            if key == '4':
-                block_pick = 4 # Pick cobblestone block
+            if key == "4":
+                block_pick = 4  # Pick cobblestone block
 
             # If 'left mouse button' is pressed, break block
-            if key == 'left mouse down':
+            if key == "left mouse down":
                 destroy(self)
 
             # If 'right mouse button' is pressed, place block
-            if key == 'right mouse down':
-                if block_pick == 1: Blocks(position = self.position + mouse.normal,
-                                            texture = grass_texture) # Place grass block
-                if block_pick == 2: Blocks(position = self.position + mouse.normal,
-                                            texture = dirt_texture)  # Place dirt block
-                if block_pick == 3: Blocks(position = self.position + mouse.normal,
-                                            texture = stone_texture) # Place stone block
-                if block_pick == 4: Blocks(position = self.position + mouse.normal,
-                                            texture = cobblestone_texture) # Place cobblestone block
+            if key == "right mouse down":
+                if block_pick == 1:
+                    Blocks(
+                        position=self.position + mouse.normal,
+                        texture=grass_texture  # Place grass block
+                    )
+                if block_pick == 2:
+                    Blocks(
+                        position=self.position + mouse.normal,
+                        texture=dirt_texture  # Place dirt block
+                    )
+                if block_pick == 3:
+                    Blocks(
+                        position=self.position + mouse.normal,
+                        texture=stone_texture  # Place stone block
+                    )
+                if block_pick == 4:
+                    Blocks(
+                        position=self.position + mouse.normal,
+                        texture=cobblestone_texture  # Place cobblestone block
+                    )
