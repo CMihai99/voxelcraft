a72df30c6bbe1fc110db5559fa6f7610c3c82cd2
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 13:07:47 +0300

    Add file which stores file component imports and links them
    Add file which stores file component imports and links file components
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

---
+++ src/__init__.py
@@ -0,0 +1,46 @@
+'''
+-----------------------------------------------------------------------------------------
+Copyright (c) 2023 Voxelcraft
+
+For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
+For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
+-----------------------------------------------------------------------------------------
+'''
+
+# Import file components
+from world.sky import Sky
+# from world.terrain import Terrain
+
+from blocks.blocks import Blocks
+from player.player import Player
+from player.arm import Arm
+from inventory.inventory import Inventory
+
+from player.actions.zoom import Zoom
+from player.actions.walk import Walk
+from player.actions.sprint import Sprint
+from player.actions.crouch import Crouch
+
+from controls import MainControls
+from blocks.controls import BlocksControls
+from player.controls import PlayerControls
+from inventory.controls import InventoryControls
+
+# Link file components
+Sky()
+# Terrain()
+
+Blocks()
+Player()
+Arm()
+Inventory()
+
+Zoom()
+Walk()
+Sprint()
+Crouch()
+
+MainControls()
+BlocksControls()
+PlayerControls()
+InventoryControls()
