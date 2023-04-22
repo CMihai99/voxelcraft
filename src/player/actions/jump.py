503f29ed5c289375aa013e3540f70a3a50c0fd0f
Calinescu Mihai <mihaimihaia431@gmail.com>
2023-04-22 16:03:20 +0300

    Format code using Black
    Format code using Black, the uncompromising Python code formatter
    
    Signed-off by: Mihai Calinescu <mihaimihaia431@gmail.com>

--- src/player/actions/jump.py
+++ src/player/actions/jump.py
@@ -1,11 +1,11 @@
-'''
+"""
 -----------------------------------------------------------------------------------------
 Copyright (c) 2023 Voxelcraft
 
 For copying notice, see https://github.com/CMihai99/voxelcraft/blob/main/COPYING.
 For licenses we use, see https://github.com/CMihai99/voxelcraft/tree/main/LICENSES.
 -----------------------------------------------------------------------------------------
-'''
+"""
 
 # Import module
 from ursina import *
@@ -13,19 +13,20 @@ from ursina import *
 # Import file components
 from main import ursina_player
 
+
 class Jump(Entity):
     def __init__(self):
         super().__init__(
-            parent = camera.ui
-            )
+            parent=camera.ui
+        )
 
     def input(self, key):
         if self.hovered:
             # If 'space' key is pressed, player is jumping
-            if key == 'space':
+            if key == "space":
                 ursina_player.jump = True
-                ursina_player.origin_y = ursina_player.origin_y + 1 # Jump 1 block
+                ursina_player.origin_y = ursina_player.origin_y + 1  # Jump 1 block
             # Player isn't jumping
             else:
                 ursina_player.jump = False
-                ursina_player.origin_y = ursina_player.origin_y # Default height
+                ursina_player.origin_y = ursina_player.origin_y  # Default height
